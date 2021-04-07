2a78d18219e5c53aebd3dac299763a7ea3cf460a
Azryel <AzryelShroud@gmail.com>
2021-04-07 18:36:31 +0200

    enable cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 require('dotenv').config()
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -23,6 +24,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//enable cors
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
