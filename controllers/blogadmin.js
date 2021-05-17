7a4e84fc5a81fc4944b16729a5cb48f3fe1ef50b
Azryel <AzryelShroud@gmail.com>
2021-05-17 18:39:36 +0200

    mock login

--- controllers/blogadmin.js
+++ controllers/blogadmin.js
@@ -1,8 +1,35 @@
 const Blogadmin = require("../models/blogadmin");
 const Blogpost = require("../models/blogpost");
 
+const bcrypt = require("bcryptjs");
+
 const { body, validationResult } = require("express-validator");
 
-exports.login = (req, res, next) => {
-   res.json("Login")
-}
+exports.login = [
+   body("user", "User cant be empty").trim().isLength({min: 1}).escape(),
+   body("password", "Password cant be empty").trim().isLength({min: 1}).escape(),
+
+   (req, res, next) => {
+
+      const errors = validationResult(req);
+      
+      if(!errors.isEmpty()) {
+         res.json(errors.array());
+     }
+     else {
+      Blogadmin.findOne({ "username": req.body.user })
+      .then(user => {
+          //mock login
+          if(req.body.password === "12345" && user!=null ) {
+            res.json("Login")
+          }
+          else {
+            res.json("Wrong password or user")
+          }
+      })
+      .catch(err => res.status(400).json('Error: ' + err));
+
+     }
+
+   }
+]
