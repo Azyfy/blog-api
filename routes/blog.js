4585e5dbb07a0850f8dba54cd994ea04b855a1ec
Azryel <AzryelShroud@gmail.com>
2021-05-26 20:31:53 +0200

    update blog route for comments

--- routes/blog.js
+++ routes/blog.js
@@ -2,8 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const blogpostsController = require("../controllers/blogposts");
+const blogcommentsController = require("../controllers/blogcomments");
 
 router.get("/", blogpostsController.get_posts);
+
+router.get("/comments", blogcommentsController.get_comments);
+
+router.post("/comments", blogcommentsController.create_comment);
 
 router.get("/:id", blogpostsController.get_single_post);
 
